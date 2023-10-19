591d96edb2450a6bf3a27882fea48450382f34dd
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-19 12:06:59 -0400

    add skills component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
 import Hero from './components/Hero';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <div className="content-divider"></div>
-      {/* <!-- skills section --> */}
-      <section className="skills clearfix">
-        {/* <!-- single skill --> */}
-        <article className="skill">
-          <span className="skill-icon">
-            <i className="fas fa-mug-hot"></i>
-          </span>
-          <h4 className="skill-title">made in USA</h4>
-          <p className="skill-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus, accusantium.
-          </p>
-        </article>
-        {/* <!-- single skill end --> */}
-        {/* <!-- single skill --> */}
-        <article className="skill">
-          <span className="skill-icon">
-            <i className="fas fa-glass-cheers"></i>
-          </span>
-          <h4 className="skill-title">relaxation</h4>
-          <p className="skill-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus, accusantium.
-          </p>
-        </article>
-        {/* <!-- single skill end --> */}
-        {/* <!-- single skill --> */}
-        <article className="skill">
-          <span className="skill-icon">
-            <i className="fas fa-bomb"></i>
-          </span>
-          <h4 className="skill-title">energy</h4>
-          <p className="skill-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus, accusantium.
-          </p>
-        </article>
-        {/* <!-- single skill end --> */}
-        {/* <!-- single skill --> */}
-        <article className="skill">
-          <span className="skill-icon">
-            <i className="fas fa-mortar-pestle"></i>
-          </span>
-          <h4 className="skill-title">family recipe</h4>
-          <p className="skill-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus, accusantium.
-          </p>
-        </article>
-        {/* <!-- single skill end --> */}
-      </section>
-      {/* <!-- skills section end --> */}
+      <Skills />
       {/* <!-- about  --> */}
       <section>
         <div className="section-center clearfix">
---
+++ src/components/Skills.js
@@ -0,0 +1,60 @@
+const Skills = () => {
+  return (
+    <div>
+      <div className="content-divider"></div>
+      {/* <!-- skills section --> */}
+      <section className="skills clearfix">
+        {/* <!-- single skill --> */}
+        <article className="skill">
+          <span className="skill-icon">
+            <i className="fas fa-mug-hot"></i>
+          </span>
+          <h4 className="skill-title">made in USA</h4>
+          <p className="skill-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus, accusantium.
+          </p>
+        </article>
+        {/* <!-- single skill end --> */}
+        {/* <!-- single skill --> */}
+        <article className="skill">
+          <span className="skill-icon">
+            <i className="fas fa-glass-cheers"></i>
+          </span>
+          <h4 className="skill-title">relaxation</h4>
+          <p className="skill-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus, accusantium.
+          </p>
+        </article>
+        {/* <!-- single skill end --> */}
+        {/* <!-- single skill --> */}
+        <article className="skill">
+          <span className="skill-icon">
+            <i className="fas fa-bomb"></i>
+          </span>
+          <h4 className="skill-title">energy</h4>
+          <p className="skill-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus, accusantium.
+          </p>
+        </article>
+        {/* <!-- single skill end --> */}
+        {/* <!-- single skill --> */}
+        <article className="skill">
+          <span className="skill-icon">
+            <i className="fas fa-mortar-pestle"></i>
+          </span>
+          <h4 className="skill-title">family recipe</h4>
+          <p className="skill-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus, accusantium.
+          </p>
+        </article>
+        {/* <!-- single skill end --> */}
+      </section>
+      {/* <!-- skills section end --> */}
+    </div>
+  );
+};
+export default Skills;
