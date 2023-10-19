fb0086c3b20f0761ff363363262f3ee4b7263ec2
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-19 11:57:41 -0400

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
+import Hero from './components/Hero';
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <!-- header --> */}
-      <header className="header">
-        <div className="banner">
-          <h2>Over one hundred flavors of</h2>
-          <h1>
-            specially <br />
-            crafted tea
-          </h1>
-          <a href="#services" className="btn banner-btn">
-            explore
-          </a>
-        </div>
-      </header>
-      {/* <!-- header end --> */}
+      <Hero />
       <div className="content-divider"></div>
       {/* <!-- skills section --> */}
       <section className="skills clearfix">
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,21 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      {/* <!-- header --> */}
+      <header className="header">
+        <div className="banner">
+          <h2>Over one hundred flavors of</h2>
+          <h1>
+            specially <br />
+            crafted tea
+          </h1>
+          <a href="#services" className="btn banner-btn">
+            explore
+          </a>
+        </div>
+      </header>
+      {/* <!-- header end --> */}
+    </>
+  );
 };
 export default Hero;
