c2b296aee815ab6954b058a1b2b7bf0617e81571
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-19 12:10:28 -0400

    add services component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
+import Services from './components/Services';
 import Skills from './components/Skills';
 
 function App() {
@@ -12,105 +13,7 @@ function App() {
       <Skills />
       <About />
       <Products />
-      {/* <!-- services --> */}
-      <section className="services" id="services">
-        {/* <!-- section title --> */}
-        <div className="section-title services-title">
-          <h3>explore</h3>
-          <h2>our services</h2>
-        </div>
-        <div className="section-center clearfix">
-          {/* <!-- card --> */}
-          <article className="service-card">
-            {/* <!-- img container --> */}
-            <div className="service-img-container">
-              {/* <!-- img --> */}
-              <img
-                src="./images/product-1.jpeg"
-                alt="single service"
-                className="service-img"
-              />
-              {/* <!-- service icon --> */}
-              <span className="service-icon">
-                <i className="fas fa-mortar-pestle fa-fw"></i>
-              </span>
-            </div>
-            {/* <!-- service info --> */}
-            <div className="service-info">
-              <h4>custom recipes</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Incidunt necessitatibus architecto ullam recusandae distinctio
-                reprehenderit.
-              </p>
-              <a href="products.html" className="btn service-btn">
-                read more
-              </a>
-            </div>
-          </article>
-          {/* <!-- card end --> */}
-          {/* <!-- card --> */}
-          <article className="service-card">
-            {/* <!-- img container --> */}
-            <div className="service-img-container">
-              {/* <!-- img --> */}
-              <img
-                src="./images/product-2.jpeg"
-                alt="single service"
-                className="service-img"
-              />
-              {/* <!-- service icon --> */}
-              <span className="service-icon">
-                <i className="fas fa-home fa-fw"></i>
-              </span>
-            </div>
-            {/* <!-- service info --> */}
-            <div className="service-info">
-              <h4>home delivery</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Incidunt necessitatibus architecto ullam recusandae distinctio
-                reprehenderit.
-              </p>
-              <a href="products.html" className="btn service-btn">
-                read more
-              </a>
-            </div>
-          </article>
-          {/* <!-- card end --> */}
-          {/* <!-- card --> */}
-          <article className="service-card">
-            {/* <!-- img container --> */}
-            <div className="service-img-container">
-              {/* <!-- img --> */}
-              <img
-                src="./images/product-3.jpeg"
-                alt="single service"
-                className="service-img"
-              />
-              {/* <!-- service icon --> */}
-              <span className="service-icon">
-                <i className="fas fa-mug-hot fa-fw"></i>
-              </span>
-            </div>
-            {/* <!-- service info --> */}
-            <div className="service-info">
-              <h4>tea aging</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Incidunt necessitatibus architecto ullam recusandae distinctio
-                reprehenderit.
-              </p>
-              <a href="products.html" className="btn service-btn">
-                read more
-              </a>
-            </div>
-          </article>
-          {/* <!-- card end --> */}
-        </div>
-        {/* <!-- section title end --> */}
-      </section>
-      {/* <!-- services end --> */}
+      <Services />
       {/* <!-- contact  --> */}
       <section className="contact">
         <div className="section-center clearfix">
